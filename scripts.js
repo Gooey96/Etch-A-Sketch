cf46e539fa03be01900fe950c2b477b097bf6831
Luqman <bluebearymuffin55@gmail.com>
2023-05-22 17:03:11 +0800

    Create function for adding and creating square div

--- scripts.js
+++ scripts.js
@@ -6,3 +6,25 @@ const clearBtn = document.querySelector('#clearBtn');
 const sizeValue = document.querySelector('#sizeValue');
 const sizeSlider = document.querySelector('#sizeSlider')
 const grid = document.querySelector('#grid');
+let boxSize = 16;
+
+createBox(boxSize);
+
+//Create square div
+function createDiv(size) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    return div;
+}
+
+//Append the square div to grid
+function createBox(gridSize) {
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            grid.appendChild(createDiv(grid.clientWidth / gridSize));
+        }
+    }
+}
+
